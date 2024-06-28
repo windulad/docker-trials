ab9db6ea0c82be0e863e770ede43b26d46b0403e
Windula Dissanayake <charithwinwindula@gmail.com>
2024-06-28 05:52:08 +0530

    initial commit

---
+++ react-app/my-app/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='header'>
+      <h1>Windula's Portfolio</h1>
+      <h5>Still under construction</h5>
+    </div>
+  );
+}
+
+export default App;
